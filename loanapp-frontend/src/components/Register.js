1e323c4545e72f4e5341d945f643e798b45cd71d
Aniruddhmuley2001 <aniruddhmuley2001@yahoo.com>
2023-09-12 10:38:38 +0530

    register page frontend

--- loanapp-frontend/src/components/Register.js
+++ loanapp-frontend/src/components/Register.js
@@ -1,37 +1,85 @@
 import { useState } from "react";
+import axios from "axios";
 
 const Register = () => {
+    const baseURL = "http://localhost:7000/saveUser";
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
     const [fullname, setFullname] = useState("")
     const [designation, setDesignation] = useState("")
     const [department, setDepartment] = useState("")
     const [dob, setDob] = useState("")
+
+    const usernameChangeHandler = (event) => {
+        setUsername(event.target.value);
+    }
+
+    const passwordChangeHandler = (event) => {
+        setPassword(event.target.value);
+    }
+
+    const fullnameChangeHandler = (event) => {
+        setFullname(event.target.value);
+    }
+
+    const designationChangeHandler = (event) => {
+        setDesignation(event.target.value);
+    }
+
+    const deptChangeHandler = (event) => {
+        setDepartment(event.target.value);
+    }
+
+    const dobChangeHandler = (event) => {
+        setDob(event.target.value);
+    }
+
+    const submitActionHandler = (event) => {
+        event.preventDefault();
+        axios
+          .post(baseURL, {
+            username: username,
+            password: password,
+            name: fullname,
+            designation: designation,
+            department:department,
+            dob: dob
+          })
+          .then((response) => {
+            alert(response.data.fullname);
+            alert("Employee "+ fullname +" added!");
+            //navigate("/account");
+          }).catch(error => {
+            alert("error==="+error);
+          });
+    
+      };
+
     return (
         <>
-        <form>
+        <form onSubmit={submitActionHandler}>
             <p>
-            <label>Username: <input type="text" value={username}></input></label>
+            <label>Username: <input type="text" value={username} onChange={usernameChangeHandler}></input></label>
             </p>
 
             <p>
-            <label>Password: <input type="password" value={password}></input></label>
+            <label>Password: <input type="password" value={password} onChange={passwordChangeHandler}></input></label>
             </p>
 
             <p>
-            <label>Full Name: <input type="text" value={fullname}></input></label>
+            <label>Full Name: <input type="text" value={fullname} onChange={fullnameChangeHandler}></input></label>
             </p>
 
             <p>
-            <label>Designation: <input type="text" value={designation}></input></label>
+            <label>Designation: <input type="text" value={designation} onChange={designationChangeHandler}></input></label>
             </p>
 
             <p>
-            <label>Department <input type="text" value={department}></input></label>
+            <label>Department <input type="text" value={department} onChange={deptChangeHandler}></input></label>
             </p>
 
             <p>
-            <label>Date of Birth: <input type="date"></input></label>
+            <label>Date of Birth: <input type="date" value={dob} onChange={dobChangeHandler}></input></label>
             </p>
 
             <button type="submit">Register</button>
